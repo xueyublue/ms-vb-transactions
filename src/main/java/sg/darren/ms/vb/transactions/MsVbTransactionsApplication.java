c69eebe346aa4f453dce5d6980f23883202e41d8
Darren <xueyublue@gmail.com>
2026-02-08 10:59:39 +0800

    init repo

---
+++ src/main/java/sg/darren/ms/vb/transactions/MsVbTransactionsApplication.java
@@ -0,0 +1,13 @@
+package sg.darren.ms.vb.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVbTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsVbTransactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sg/darren/ms/vb/transactions/MsVbTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.darren.ms.vb.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVbTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
